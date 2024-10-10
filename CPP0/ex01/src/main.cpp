fc080efe70297f8678dcd3f38b8bf3a80a772375
Victor Philippe <me@ktorvi.me>
2024-10-10 19:53:49 +0200

    finished cpp0

---
+++ CPP0/ex01/src/main.cpp
@@ -0,0 +1,34 @@
+#include "../includes/Phonebook.hpp"
+#include <iostream>
+int main(int argc, char **argv)
+{
+    Phonebook phonebook;
+    int count = 0;
+
+    (void)argv;
+    if (argc == 1)
+    {
+        while(1)
+        {
+            std::cout << "Enter command: ";
+            std::string command;
+            std::cin >> command;
+            if(command == "SEARCH")
+            {
+                phonebook.printPhonebook();
+            }
+            else if(command == "ADD")
+            {
+                phonebook.addContact(&count);
+            }
+            else if(command == "EXIT")
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Usage: ./phonebook" << std::endl;
+    }
+}
